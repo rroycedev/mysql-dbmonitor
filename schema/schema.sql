0467c727c19ce4a9088536e0ebf8f24fa98727fa
Unknown <rroycedev@gmail.com>
2018-12-12 16:52:17 -0500

    Initial version

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.16  Distrib 10.2.11-MariaDB, for osx10.13 (x86_64)
+-- MySQL dump 10.15  Distrib 10.0.36-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: dbmonitor
+-- Host: localhost    Database: mdbadmin
 -- ------------------------------------------------------
--- Server version	10.2.11-MariaDB-log
+-- Server version	10.0.36-MariaDB-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -131,9 +131,9 @@ DROP TABLE IF EXISTS `dns_aliases`;
 CREATE TABLE `dns_aliases` (
   `alias_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
-  `is_vip` tinyint(3) unsigned NOT NULL DEFAULT 0,
-  `vip_port` int(10) unsigned NOT NULL DEFAULT 0,
-  `active` tinyint(3) unsigned NOT NULL DEFAULT 1,
+  `is_vip` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `vip_port` int(10) unsigned NOT NULL DEFAULT '0',
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`alias_id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
@@ -176,6 +176,56 @@ INSERT INTO `environments` VALUES (31,'Production',1),(131,'QA',2),(231,'Develop
 UNLOCK TABLES;
 
 --
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+INSERT INTO `password_resets` VALUES ('ruhruhroy@gmail.com','$2y$10$QbrXACAFDk9YbU4d6gTe6OGI.9WXcPYC3EIWCxL7Ik8oRzyUfHlqu','2018-12-13 02:28:42');
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `replication_lag_alerts`
 --
 
@@ -213,8 +263,8 @@ CREATE TABLE `server_slaves_status` (
   `connection_name` varchar(60) NOT NULL,
   `master_binlog_position` bigint(20) unsigned NOT NULL,
   `master_binlog_filename` varchar(100) NOT NULL,
-  `lag_time_secs` bigint(20) unsigned NOT NULL DEFAULT 0,
-  `check_datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `lag_time_secs` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `check_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`server_slave_status_id`),
   UNIQUE KEY `idx_server_id_connection_name` (`server_id`,`connection_name`),
   CONSTRAINT `fk_server_id` FOREIGN KEY (`server_id`) REFERENCES `servers` (`server_id`) ON DELETE CASCADE ON UPDATE NO ACTION
@@ -244,7 +294,7 @@ CREATE TABLE `server_status` (
   `connection_count` int(10) unsigned NOT NULL,
   `cpu_load` decimal(10,2) unsigned NOT NULL,
   `disk_info` text NOT NULL,
-  `check_datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `check_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`server_status_id`),
   UNIQUE KEY `server_id` (`server_id`),
   CONSTRAINT `fk_server_status_server_id` FOREIGN KEY (`server_id`) REFERENCES `servers` (`server_id`) ON DELETE CASCADE ON UPDATE NO ACTION
@@ -257,7 +307,7 @@ CREATE TABLE `server_status` (
 
 LOCK TABLES `server_status` WRITE;
 /*!40000 ALTER TABLE `server_status` DISABLE KEYS */;
-INSERT INTO `server_status` VALUES (1,9711,34,3.45,'[{\"volume\":\"/opt/mysql/data\",\"space_used\":\"600GB\",\"space_available\":\"424GB\",\"total_space\":\"1024GB\",\"percent_used\":66,\"percent_free\":34},{\"volume\":\"/opt/mysql/tmp\",\"space_used\":\"10GB\",\"space_available\":\"20GB\",\"total_space\":\"30GB\",\"percent_used\":34,\"percent_free\":66}]','2018-12-08 09:24:50'),(2,9712,34,3.45,'[{\"volume\":\"/opt/mysql/data\",\"space_used\":\"600GB\",\"space_available\":\"424GB\",\"total_space\":\"1024GB\",\"percent_used\":66,\"percent_free\":34},{\"volume\":\"/opt/mysql/tmp\",\"space_used\":\"10GB\",\"space_available\":\"20GB\",\"total_space\":\"30GB\",\"percent_used\":34,\"percent_free\":66}]','2018-12-08 09:25:18');
+INSERT INTO `server_status` VALUES (1,9711,39,3.45,'[{\"volume\":\"/opt/mysql/data\",\"space_used\":\"600GB\",\"space_available\":\"424GB\",\"total_space\":\"1024GB\",\"percent_used\":66,\"percent_free\":34},{\"volume\":\"/opt/mysql/tmp\",\"space_used\":\"10GB\",\"space_available\":\"20GB\",\"total_space\":\"30GB\",\"percent_used\":34,\"percent_free\":66}]','2018-12-08 09:24:50'),(2,9712,39,3.45,'[{\"volume\":\"/opt/mysql/data\",\"space_used\":\"600GB\",\"space_available\":\"424GB\",\"total_space\":\"1024GB\",\"percent_used\":66,\"percent_free\":34},{\"volume\":\"/opt/mysql/tmp\",\"space_used\":\"10GB\",\"space_available\":\"20GB\",\"total_space\":\"30GB\",\"percent_used\":34,\"percent_free\":66}]','2018-12-08 09:25:18');
 /*!40000 ALTER TABLE `server_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,11 +326,11 @@ CREATE TABLE `servers` (
   `environment_id` bigint(20) unsigned NOT NULL,
   `datacenter_id` bigint(20) unsigned NOT NULL,
   `cluster_id` bigint(20) unsigned NOT NULL,
-  `view_order` int(11) NOT NULL DEFAULT 1 COMMENT 'Order to display in front-end.  Values start at 1',
-  `status` tinyint(3) unsigned NOT NULL DEFAULT 1 COMMENT 'Values are 0 (inactive), 1 (active), 2 (maintenance)',
-  `exclude_noc` tinyint(4) unsigned NOT NULL DEFAULT 0 COMMENT 'Values are 0 or 1',
-  `exclude_disk_check` tinyint(4) NOT NULL DEFAULT 0,
-  `maint_mode` tinyint(4) unsigned NOT NULL DEFAULT 0 COMMENT 'Values are 0 or 1',
+  `view_order` int(11) NOT NULL DEFAULT '1' COMMENT 'Order to display in front-end.  Values start at 1',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `exclude_noc` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT 'Values are 0 or 1',
+  `exclude_disk_check` tinyint(4) NOT NULL DEFAULT '0',
+  `maint_mode` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT 'Values are 0 or 1',
   `maint_mode_start_date` datetime DEFAULT NULL,
   PRIMARY KEY (`server_id`),
   UNIQUE KEY `hostname_port_name` (`hostname`,`port_name`),
@@ -300,8 +350,38 @@ CREATE TABLE `servers` (
 
 LOCK TABLES `servers` WRITE;
 /*!40000 ALTER TABLE `servers` DISABLE KEYS */;
-INSERT INTO `servers` VALUES (9711,'dbserver1','','10.0.0.29',31,31,31,1,1,0,0,0,NULL),(9712,'dbserver2','','10.0.0.31',31,31,31,1,1,0,0,0,NULL),(9713,'dbserver3','','10.0.0.31:3307',31,31,31,1,1,0,0,0,NULL),(9714,'dbserver4','','10.0.0.4',31,31,31,1,1,0,0,0,NULL),(9715,'dbserver5','','10.0.0.5',31,31,31,1,1,0,0,0,NULL),(9716,'dbserver6','','10.0.0.6',31,31,31,1,1,0,0,0,NULL),(9717,'dbserver7','','10.0.0.7',31,31,31,1,1,0,0,0,NULL),(9718,'dbserver8','','10.0.0.8',31,31,31,1,1,0,0,0,NULL),(9719,'dbserver9','','10.0.0.9',31,31,31,1,1,0,0,0,NULL);
+INSERT INTO `servers` VALUES (9711,'dbserver1','','10.0.0.29',31,31,31,1,0,0,0,0,NULL),(9712,'dbserver2','','10.0.0.31',31,31,31,1,1,0,0,0,NULL),(9713,'dbserver3','','10.0.0.31:3307',31,31,31,1,1,0,0,0,NULL),(9714,'dbserver4','','10.0.0.4',31,31,31,1,0,0,0,0,NULL),(9715,'dbserver5','','10.0.0.5',31,31,31,1,1,0,0,0,NULL),(9716,'dbserver6','','10.0.0.6',31,31,31,1,1,0,0,0,NULL),(9717,'dbserver7','','10.0.0.7',31,31,31,1,2,0,0,0,NULL),(9718,'dbserver8','','10.0.0.8',31,31,31,1,2,0,0,0,NULL),(9719,'dbserver9','','10.0.0.9',31,31,31,1,2,0,0,0,NULL);
 /*!40000 ALTER TABLE `servers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Ron Royce','ruhruhroy@gmail.com','$2y$10$yCofxQ2Lh6z8mF.RIpkzMe2qpEwnX0okNJJw7sQW0okIwSTHf4zoa','NBvTgYVfv6FH1MS4WvlxswSc9oKzjzmgY9Sk7Wg6AHtKLtc9mAuXstYybtC4','2018-12-13 00:12:04','2018-12-13 00:12:04');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -313,4 +393,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-09 10:16:34
+-- Dump completed on 2018-12-12 16:50:38
